157d8d270d57341f1f927a35608aa2e2aba8535a
pawel <pawel@pawel-Vostro-3700>
2016-01-09 22:27:40 +0100

    fix study

---
+++ src/main/webapp/resources/js/app/views/desktop/study.js
@@ -0,0 +1,36 @@
+/**
+ * The Study view
+ */
+define([
+    'utilities',
+    'text!../../../../templates/desktop/study.html'
+], function (utilities, StudyTemplate) {
+
+    var StudyView = Backbone.View.extend({
+    	events: {
+    		"click #button-start": "startClicked",
+        },
+        
+        render:function () {
+        /*	
+         * console.log(JSON.stringify(this.model.next()));
+         */
+            utilities.applyTemplate($(this.el),StudyTemplate,{model:this.model});
+            return this;
+        },
+        
+        startClicked: function(e){
+        	e.preventDefault();
+        	window.location.hash = 'learning/'+this.categoryId;
+        },
+        setCategoryId: function(categoryId){
+        	this.categoryId = parseInt(categoryId);
+        	return this;
+        }
+        
+    
+    
+    });
+
+    return StudyView;
+});
